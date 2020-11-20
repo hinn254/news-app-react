a5b78f60e632f7009057749364488c7640e7cb79
Benny <bennyhinnotieno@gmail.com>
2020-11-20 14:53:36 +0300

    finalised app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import NewsDisplay from "./NewsDisplay";
+import "./App.css";
+
+const url =
+  "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e4922320902e4ac693248a71181af89b";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetch(url);
+      const result = await resp.json();
+      setData(result);
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
+  if (data) {
+    return (
+      <div className="App">
+        <h1>Welcome News App</h1>
+
+        {data.articles.map((news) => (
+          <NewsDisplay key={news.url} news={news} />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1>Sorry no data received</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
